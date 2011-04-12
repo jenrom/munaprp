5b65757e20755c77fbc76373f0c09a317223446e
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-04-12 06:58:01 -0400

    Updated

--- source/nothinbutdotnetprep/infrastructure/filtering/FilteringExtensions.cs
+++ source/nothinbutdotnetprep/infrastructure/filtering/FilteringExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using nothinbutdotnetprep.infrastructure.ranges;
+
 namespace nothinbutdotnetprep.infrastructure.filtering
 {
     public static class FilteringExtensions
@@ -17,6 +20,27 @@ namespace nothinbutdotnetprep.infrastructure.filtering
         {
             return new PropertyCriteria<ItemToMatch, PropertyType>(criteria,
                                                                    filtering_extension_point.accessor);
+        }
+        public static Criteria<ItemToMatch> greater_than_or_equal_to<ItemToMatch,PropertyType>(this FilteringExtensionPoint<ItemToMatch,PropertyType> extension_point,PropertyType value) where PropertyType : IComparable<PropertyType>
+        {
+            return matches(extension_point,new FallsInRange<PropertyType>(new RangeWithNoUpperBound<PropertyType>(value))
+                               .or(new EqualToAny<PropertyType>(value)));
+        }
+
+        public static Criteria<ItemToMatch> less_than_or_equal_to<ItemToMatch,PropertyType>(this FilteringExtensionPoint<ItemToMatch,PropertyType> extension_point,PropertyType value) where PropertyType : IComparable<PropertyType>
+        {
+            return matches(extension_point,new FallsInRange<PropertyType>(new RangeWithNoLowerBound<PropertyType>(value))
+                               .or(new EqualToAny<PropertyType>(value)));
+        }
+
+        public static Criteria<ItemToMatch> between<ItemToMatch,PropertyType>(this FilteringExtensionPoint<ItemToMatch,PropertyType> extension_point,PropertyType start, PropertyType end) where PropertyType : IComparable<PropertyType>
+        {
+            return greater_than_or_equal_to(extension_point,start).and(less_than_or_equal_to(extension_point,end));
+        }
+
+        public static Criteria<ItemToMatch> greater_than<ItemToMatch,PropertyType>(this FilteringExtensionPoint<ItemToMatch,PropertyType> extension_point,PropertyType value) where PropertyType : IComparable<PropertyType>
+        {
+            return matches(extension_point,new FallsInRange<PropertyType>(new RangeWithNoUpperBound<PropertyType>(value)));
         }
     }
 }
