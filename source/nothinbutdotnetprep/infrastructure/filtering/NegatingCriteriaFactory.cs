09bf79e430fb1681b422be5158bbd2289ece871d
Jan Feilen <jfeilen@autoscout24.com>
2011-04-12 12:13:10 +0200

    Pushing new changes

---
+++ source/nothinbutdotnetprep/infrastructure/filtering/NegatingCriteriaFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetprep.infrastructure.filtering
+{
+    public class NegatingCriteriaFactory<ItemToMatch, PropertyType> : ICreateSpecifications<ItemToMatch, PropertyType>
+    {
+        ICreateSpecifications<ItemToMatch, PropertyType> factory;
+
+        public NegatingCriteriaFactory(ICreateSpecifications<ItemToMatch, PropertyType> factory)
+        {
+            this.factory = factory;
+        }
+
+        public Criteria<ItemToMatch> equal_to(PropertyType value)
+        {
+            return negate(factory.equal_to(value));
+        }
+
+        public Criteria<ItemToMatch> equal_to_any(params PropertyType[] values)
+        {
+            return negate(factory.equal_to_any(values));
+        }
+
+        public Criteria<ItemToMatch> not_equal_to(PropertyType value)
+        {
+            return negate(factory.not_equal_to(value));
+        }
+
+        public Criteria<ItemToMatch> matches(Criteria<PropertyType> itemToMatch)
+        {
+            return negate(factory.matches(itemToMatch));
+        }
+
+        private static Criteria<ItemToMatch> negate(Criteria<ItemToMatch> criteria)
+        {
+            return new NegatingCriteria<ItemToMatch>(criteria);
+        }
+
+    }
+}
