09598271b5e3f4c30398314c4508251654213dd9
Your Name <you@email.com>
2011-04-13 09:46:26 +0200

    Pushing new changes

--- source/nothinbutdotnetprep/infrastructure/sorting/Sort.cs
+++ source/nothinbutdotnetprep/infrastructure/sorting/Sort.cs
@@ -8,24 +8,19 @@ namespace nothinbutdotnetprep.infrastructure.sorting
     {
         public static ComparerBuilder<ItemToSort> by<PropertyType>(PropertyAccessor<ItemToSort, PropertyType> accessor, params PropertyType[] values)
         {
-            return create_builder().then_by(accessor, values);
+            return new ComparerBuilder<ItemToSort>().then_by(accessor, values);
         }
 
         public static ComparerBuilder<ItemToSort> by_descending<PropertyType>(PropertyAccessor<ItemToSort, PropertyType> accessor)
             where PropertyType : IComparable<PropertyType>
         {
-            return create_builder().then_by_descending(accessor);
+            return new ComparerBuilder<ItemToSort>().then_by_descending(accessor);
         }
 
         public static ComparerBuilder<ItemToSort> by<PropertyType>(PropertyAccessor<ItemToSort, PropertyType> accessor)
             where PropertyType : IComparable<PropertyType>
         {
-            return create_builder().then_by(accessor);
-        }
-
-        private static ComparerBuilder<ItemToSort> create_builder()
-        {
-            return new ComparerBuilder<ItemToSort>();
+            return new ComparerBuilder<ItemToSort>().then_by(accessor);
         }
     }
 }
